426132cc15d68a51f1946a66533f841bc50ea674
akashk11 <akashvinod777@gmail.com>
2025-12-18 04:41:02 +0400

    rule meta controller and service added

--- src/rules/rules.module.ts
+++ src/rules/rules.module.ts
@@ -5,11 +5,18 @@ import { DatabaseModule } from 'src/database/database.module';
 import { DryRunModule } from 'src/dry-run/dry-run.module';
 import { QueryBuilderService } from 'src/common/rules/query-builder/query-builder.service';
 import { RulesSchedulerService } from './rules.scheduler';
+import { RulesMetaService } from './rule.meta.service';
+import { RulesMetaController } from './rule.meta.controller';
 
 @Module({
   imports: [DatabaseModule, DryRunModule], // ✅ bring in DryRunService
 
-  providers: [RulesService, QueryBuilderService, RulesSchedulerService],
-  controllers: [RulesController],
+  providers: [
+    RulesService,
+    QueryBuilderService,
+    RulesSchedulerService,
+    RulesMetaService,
+  ],
+  controllers: [RulesController, RulesMetaController],
 })
 export class RulesModule {}
