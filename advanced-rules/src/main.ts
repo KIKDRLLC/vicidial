994dede24f67f951aeda293dec1673c540eb95b7
Akash K V <akvinod@hustlehubbpo.com>
2026-02-04 05:47:09 +0530

    feat: add crypto polyfill for globalThis in main.ts

--- advanced-rules/src/main.ts
+++ advanced-rules/src/main.ts
@@ -1,6 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as nodeCrypto from 'crypto';
+
+if (!(globalThis as any).crypto) {
+  (globalThis as any).crypto = nodeCrypto;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
